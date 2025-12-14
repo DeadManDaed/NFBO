10e4dffd64e2b47c9068471104b62c5cb338583c
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2025-12-14 11:16:50 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // server/server.js
 require('dotenv').config();
 const http = require('http');
+const path = require('path');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
@@ -11,7 +12,7 @@ server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT} (env=${process.env.NODE_ENV || 'development'})`);
 });
 
-// gestion propre du shutdown
+// Gestion propre du shutdown
 process.on('SIGINT', () => {
   console.log('SIGINT received, shutting down');
   server.close(() => process.exit(0));
