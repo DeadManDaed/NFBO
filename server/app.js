91f779d075104f7ed29c47dbe504c2f19dfce118
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2025-12-23 01:08:55 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ const admissionsRouter = require('./routes/admissions');
 const retraitsRouter = require('./routes/retraits');
 const employersRouter = require('./routes/employers');
 const magasinsRoutes = require('./routes/magasins');
+const transfertsRoutes = require('./server/transferts/pending-audit');
 //const errorsRoute = require('./logs/errors');
 
 const app = express();
@@ -40,6 +41,7 @@ app.use('/api/employers', employersRouter);
 app.use('/api/magasins', magasinsRoutes);
 app.use('/api/geo', geoRoutes);
 //app.use('/api/errors', errorsRoute);
+app.use('/api/transferts', transfertsRoutes);
 
 // 5. ROUTES DE BASE
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
@@ -61,5 +63,6 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
 
 
