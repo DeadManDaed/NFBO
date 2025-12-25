4066ef3be8409a29045f39fcd1ab32cde4eda7fb
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2025-12-25 07:55:13 +0100

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -26,7 +26,7 @@ router.get('/magasin/:magasinId', async (req, res) => {
 });
 
 // POST nouvel utilisateur
-rooter.post('/api/users', async (req, res) => {
+router.post('/api/users', async (req, res) => {
     const { username, password, role, prenom, nom, email, telephone, magasin_id, statut } = req.body;
     
     console.log('🔵 Création utilisateur:', username, role);
