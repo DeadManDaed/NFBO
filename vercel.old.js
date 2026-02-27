8abd55f9ebd045b7196c82fb08ccf4ce791e7bf1
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-02-27 19:51:31 +0100

    Update vercel.old.js

--- vercel.old.js
+++ vercel.old.js
@@ -10,3 +10,22 @@
     { "src": "/(.*)", "dest": "/index.html" }
   ]
 }
+
+
+
+//VERSION QUI FONCTIONNE AVANT SUGGESTION DE SIMPLIFICATION GEM
+
+{
+  "version": 2,
+  "builds": [
+    { "src": "package.json", "use": "@vercel/static-build", "config": { "distDir": "dist" } },
+    { "src": "server/server.js", "use": "@vercel/node" }
+  ],
+  "routes": [
+    { "src": "/api/(.*)", "dest": "server/server.js" },
+    { "src": "/assets/(.*)", "dest": "/assets/$1" },
+    { "src": "/icons/(.*)", "dest": "/icons/$1" },
+    { "src": "/manifest.json", "dest": "/manifest.json" },
+    { "src": "/(.*)", "dest": "/index.html" }
+  ]
+}
