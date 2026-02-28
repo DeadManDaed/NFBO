a55c890c553db31d05959eacdf82873164160904
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-02-28 09:55:56 +0100

    Update Administration.jsx

--- src/pages/Administration.jsx
+++ src/pages/Administration.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
+import PageLayout, { StateLoading, StateEmpty } from '../components/PageLayout';
 
 // ─── CONSTANTES ────────────────────────────────────────────────────────────────
 
