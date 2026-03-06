0b59381783ae71061aa43014ecb7192b9663a171
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-03-06 18:13:01 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
-// api/index.js
-const app = require('../server/app'); // On importe ton app Express existante
+const { requireAuth } = require('./_lib/auth');
+const { withCors }    = require('./_lib/cors');
 
-// Vercel a besoin que l'on exporte l'instance d'Express
-module.exports = app;
+const routes = {
+  '/api/admissions':        () => require('./admissions'),
+  '/api/audit':             () => require('./audit'),
+  '/api/geo':               () => require('./geo'),
+  '/api/lots':              () => require('./lots/index'),
+  '/api/magasins':          () => require('./magasins'),
+  '/api/messages':          () => require('./messages'),
+  '/api/operations_caisse': () => require('./operations_caisse'),
+  '/api/personnel':         () => require('./personnel/index'),
+  '/api/employers':         () => require('./personnel/index'),
+  '/api/users':             () => require('./personnel/index'),
+  '/api/producteurs':       () => require('./producteurs'),
+  '/api/retraits':          () => require('./retraits'),
+  '/api/transferts':        () => require('./transferts'),
+  '/api/auth':              () => require('./auth/index'),
+};
