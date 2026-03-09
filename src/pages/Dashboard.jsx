5fe8221c402f009baa411614de2a7e25a5cd13aa
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-03-09 12:36:09 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -16,6 +16,7 @@ const TABS = [
   { id:'retraits',      label:'Sorties',   icon: Icons.RetIcon,   roles:['superadmin','admin','stock'] },
   { id:'transferts',    label:'Transf.',   icon: Icons.TransfIcon,roles:['superadmin','admin'] },
   { id:'stock',         label:'Stock',     icon: Icons.StockIcon, roles:['superadmin','admin','stock','caisse'] },
+  { id:'caisse',        label:'Caisse',    icon: Icons.CaisseIcon,roles:['superadmin','admin','gestionnaire','caisse'] },
   { id:'audit',         label:'Audit',     icon: Icons.AuditIcon, roles:['superadmin','auditeur'] },
   { id:'messagerie',    label:'Messages',  icon: Icons.MsgIcon,   roles:['all'] },
   { id:'administration',label:'Admin',     icon: Icons.AdminIcon, roles:['superadmin','admin'] },
