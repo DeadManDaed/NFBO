8fb074cc5f9f0ef364c45b4193bff07c13be1178
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-03-21 10:26:44 +0100

    Update Admissions.jsx

--- src/pages/Admissions.jsx
+++ src/pages/Admissions.jsx
@@ -1,5 +1,6 @@
 // src/pages/Admissions.jsx
 
+
 import { useState, useEffect, useCallback } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useAuth } from '../hooks/useAuth';
